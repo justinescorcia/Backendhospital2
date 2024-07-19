f1cdb7136f72d659bb24bab3d2da0f4ab26f0148
Justin Martin Muñoz Escorcia <210659@utxicotepec.edu.mx>
2024-07-19 14:10:11 -0600

    mas correccion

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_wPQL-xDZFWew0WDOq7j@basebackend-utxicotepec-efe2.h.aivencloud.com:19575/defaultdb?ssl-mode=REQUIRED"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_wPQL-xDZFWew0WDOq7j@basebackend-utxicotepec-efe2.h.aivencloud.com:19575/defaultdb"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
